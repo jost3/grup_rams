871b6c624f5e65bffb28c60610f11ae2397fb9b5
jost3 <1427593@senati.pe>
2023-08-17 14:54:58 -0500

    se agrega las 2 ultimas operaciones

--- op2/src/main/java/com/mycompany/op2/Op2.java
+++ op2/src/main/java/com/mycompany/op2/Op2.java
@@ -14,7 +14,36 @@ public class Op2 {
         //Creacion de Variables
         
         int n1,n2,suma,resta,mult,div;
-       
-        
-    }
+       String op;
+        switch(op){
+         
+            //multiplicacion
+            case "3":
+                System.out.println("\t multiplicacion \n ingresa el primer numero: ");
+                n1 = a1.nextInt();
+                System.out.println("ingesa el segundo numero: ");
+                n2 = a1.nextInt();
+                
+                mult = n1 * n2;
+                
+                System.out.println("La multiplicacion es : "+mult+"\n");
+                break;
+            //division
+            case "4":
+                System.out.println("\t Division \n ingresa el primer numero: ");
+                n1 = a1.nextInt();
+                System.out.println("ingesa el segundo numero: ");
+                n2 = a1.nextInt();
+                
+                div = n1 / n2;
+                
+                System.out.println("La division es : "+div+"\n");
+                break;
+                
+            default:
+                System.out.println("\n OPCION NO VALIDA");
+                break;
+    }while(10==10);
 }
+}
+        
