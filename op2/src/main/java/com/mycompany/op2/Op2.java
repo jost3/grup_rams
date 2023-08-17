66922007833a738ba6094c5a70e1e9a8e7a20253
RosalesJhordi <1428060@senati.pe>
2023-08-17 14:29:02 -0500

    Importacion de clase scanner y primera visualizacion

--- op2/src/main/java/com/mycompany/op2/Op2.java
+++ op2/src/main/java/com/mycompany/op2/Op2.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.op2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alumno
@@ -15,5 +17,11 @@ public class Op2 {
         
         int n1,n2,suma,resta,mult,div;
         String op;
+        
+        // importamos la clase scanner
+        Scanner a1 = new Scanner(System.in);
+        // Priemra visualizacion
+        System.out.print("INGRESE SU OPCION \n 1: SUMA \n 2: RESTA \n 3: MULTIPLICACION \n 3: DIVISION \n Opcion: ");
+        op = a1.next();
     }
 }
